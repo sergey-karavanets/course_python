5c1409a48d123def47684a53d705a3cdd37e3fd4
karavanets <dront22rus@mail.ru>
2022-09-13 16:02:13 +0700

    add a solution to problem 15.5 step 10

--- Функции/Функции высшего порядка/Programming/task.py
+++ Функции/Функции высшего порядка/Programming/task.py
@@ -4,5 +4,9 @@ def map(function, items):
         result.append(function(item))
     return result
 
+def round_number(num):
+    return round(num, 2)
 
 numbers = [3.56773, 5.57668, 4.00914, 56.24241, 9.01344, 32.12013, 23.22222, 90.09873, 45.45, 314.1528, 2.71828, 1.41546]
+
+print(*map(round_number, numbers), sep='\n')
