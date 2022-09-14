4eb79221d7f7907b7070397818ea0c4e80e2d80e
karavanets <dront22rus@mail.ru>
2022-09-14 17:45:50 +0700

    add a solution to problem 15.9 step 14

--- Функции/Встроенные функции any(), all(), zip(), enumerate()/Programming (6)/task.py
+++ Функции/Встроенные функции any(), all(), zip(), enumerate()/Programming (6)/task.py
@@ -1,1 +1,5 @@
-# put your python code here
+progress = []
+for i in range(int(input())):
+    progress.append(any(['5' in input().split() for j in range(int(input()))]))
+
+print('YES' if all(progress) else 'NO')
