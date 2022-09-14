ce6562715451d369107fca13d361fd0233484098
karavanets <dront22rus@mail.ru>
2022-09-14 16:19:08 +0700

    add a solution to problem 15.9 step 10

--- Функции/Встроенные функции any(), all(), zip(), enumerate()/Programming/task.py
+++ Функции/Встроенные функции any(), all(), zip(), enumerate()/Programming/task.py
@@ -2,3 +2,4 @@ def ignore_command(command):
     ignore = ['alias', 'configuration', 'ip', 'sql', 'select', 'update', 'exec', 'del', 'truncate']
 
     return any(map(lambda x: x in command, ignore))
+
