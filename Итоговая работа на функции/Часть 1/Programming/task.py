d8c57f2f2a8c88c861ee2a19ebfc094d33e699e7
karavanets <dront22rus@mail.ru>
2022-09-15 10:56:57 +0700

    add a solution to problem 16.1 step 15

--- Итоговая работа на функции/Часть 1/Programming/task.py
+++ Итоговая работа на функции/Часть 1/Programming/task.py
@@ -1,2 +1,7 @@
-def generate_letter(mail, name, date, time, place, teacher, number):
-    pass
+def generate_letter(mail, name, date, time, place, teacher='Тимур Гуев', number=17):
+    return (f'To: {mail}\n'
+    f'Приветствую, {name}!\n' 
+    f'Вам назначен экзамен, который пройдет {date}, в {time}.\n' 
+    f'По адресу: {place}.\n'
+    f'Экзамен будет проводить {teacher} в кабинете {number}.\n'
+    f'Желаем удачи на экзамене!')
