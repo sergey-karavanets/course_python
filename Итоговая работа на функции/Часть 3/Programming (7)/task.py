40cebaf45458c847e21224e05fc407c76d8e0751
karavanets <dront22rus@mail.ru>
2022-09-15 12:53:10 +0700

    add a solution to problem 16.3 step 8

--- Итоговая работа на функции/Часть 3/Programming (7)/task.py
+++ Итоговая работа на функции/Часть 3/Programming (7)/task.py
@@ -1,2 +1,7 @@
+from operator import *
 def arithmetic_operation(operation):
-    pass
+
+    def func(x, y):
+        result = {'+': add(x, y), '-': sub(x, y), '*': mul(x, y), '/': truediv(x, y)}
+        return result[operation]
+    return func
