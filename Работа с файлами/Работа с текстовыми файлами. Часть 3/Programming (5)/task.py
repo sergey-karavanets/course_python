c9109e96bbaa3237d486c48b81de236d4b0e085f
karavanets <dront22rus@mail.ru>
2022-09-24 13:23:16 +0700

    add solution to problem 17.4 step 13

--- Работа с файлами/Работа с текстовыми файлами. Часть 3/Programming (5)/task.py
+++ Работа с файлами/Работа с текстовыми файлами. Часть 3/Programming (5)/task.py
@@ -1,1 +1,7 @@
-# put your python code here
+output = open('output.txt', 'w', encoding='utf-8')
+
+for _ in range(int(input())):
+    with open(input(), encoding='utf-8') as file:
+        output.writelines(file)
+
+output.close()
