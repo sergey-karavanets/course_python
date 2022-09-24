40a48205fb5191a771c4fc0a62ece729d1af4eec
karavanets <dront22rus@mail.ru>
2022-09-24 13:45:50 +0700

    add solution to problem 17.4 step 14

--- Работа с файлами/Работа с текстовыми файлами. Часть 3/Programming (6)/task.py
+++ Работа с файлами/Работа с текстовыми файлами. Часть 3/Programming (6)/task.py
@@ -1,1 +1,7 @@
-# put your python code here
+with open('logfile.txt', encoding='utf-8') as logfile, open('output.txt', 'w', encoding='utf-8') as output:
+    temp = []
+    for log in logfile:
+        temp.append(log.strip().split(', '))
+    for name, start, stop in temp:
+        if (int(stop.split(':')[0]) * 60 + int(stop.split(':')[1])) - (int(start.split(':')[0]) * 60 + int(start.split(':')[1])) >= 60:
+            print(name, file=output)
